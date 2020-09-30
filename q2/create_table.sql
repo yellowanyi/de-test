574a0e642f560a76fab90e6958ccb99e39f28801
wanyi <huangwanyi@me.com>
2020-10-01 01:44:37 +0800

    update schema design

--- q2/create_table.sql
+++ q2/create_table.sql
@@ -11,28 +11,34 @@ CREATE TABLE customer
      phone TEXT NOT NULL
   );
 
+CREATE TABLE manufacturer
+  (
+     id   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+     name TEXT
+  );
+
 CREATE TABLE car
   (
      id                    INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
      is_new                BOOLEAN NOT NULL DEFAULT true,
-     manufacturer          TEXT NOT NULL,
+     manufacturer_id       INTEGER,
      model_name            TEXT NOT NULL,
      model_variant         TEXT NOT NULL,
      serial_number         TEXT NOT NULL,
      weight                TEXT NOT NULL,
      engine_cubic_capacity TEXT NOT NULL,
-     price                 DECIMAL(18, 8) NOT NULL
+     price                 DECIMAL(18, 8) NOT NULL,
+     CONSTRAINT fk_manufacturer FOREIGN KEY (manufacturer_id) REFERENCES manufacturer (id)
   );
 
 CREATE TABLE transaction
   (
-     id               INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
      salesperson_id   INTEGER,
-     customer_id      INTEGER,
      car_id           INTEGER,
-     status           TEXT,
+     PRIMARY KEY      (salesperson_id, car_id),
+     customer_id      INTEGER,
      create_timestamp TIMESTAMP NOT NULL DEFAULT Now(),
      CONSTRAINT fk_salesperson FOREIGN KEY (salesperson_id) REFERENCES salesperson (id),
-     CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer (id),
-     CONSTRAINT fk_car FOREIGN KEY (car_id) REFERENCES car (id)
+     CONSTRAINT fk_car FOREIGN KEY (car_id) REFERENCES car (id),
+     CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer (id)
   );
